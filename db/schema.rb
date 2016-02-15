dc6a9f8dcee2efca8be5d598c0cd5d6d91d868e3
Robert Cunningham <rgc3dev@gmail.com>
2016-02-15 06:57:45 -0500

    Add price and interior color columns to vehicles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160208021313) do
+ActiveRecord::Schema.define(version: 20160215115632) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -49,6 +49,8 @@ ActiveRecord::Schema.define(version: 20160208021313) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.decimal  "price"
+    t.string   "interior_color"
   end
 
 end
